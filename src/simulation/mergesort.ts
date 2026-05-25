092b3f3624e3adfca184cd650d020eff824bfd6d
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2026-05-25 00:29:49 -0400

    Attribute an id to each node and implement visibility mutation methods for the nodes and a single value

--- src/simulation/mergesort.ts
+++ src/simulation/mergesort.ts
@@ -1,13 +1,44 @@
+export interface ElementProp {
+  value: number
+  elementIndex: number
+  visible: boolean
+  state?: 'select min' | 'proccessed'
+}
+
 export interface TreeNodeProp {
-  A: number[]
+  id: number
+  A: ElementProp[]
   left: TreeNodeProp | null
   right: TreeNodeProp | null
+  visible: boolean;
+  state?: 'select'
 }
 
 export function buildTree(A: number[]): TreeNodeProp {
-  const root: TreeNodeProp = {A: A, left: null, right: null}
+  const elements: ElementProp[] = A.map((value, index) => {
+    return {value: value, elementIndex: index, visible: true}
+  })
+  const root: TreeNodeProp = {id: -1, A: elements, left: null, right: null, visible: true}
 
-  function buildTreeHelper(A: number[], root: TreeNodeProp): TreeNodeProp | null {
+  const generateId = (root: TreeNodeProp | null) => {
+    const queue: (TreeNodeProp | null)[] = [root]
+    let counter = 1
+
+    while (queue.length > 0) {
+      const node: TreeNodeProp | null | undefined = queue.shift()
+      if (node) {
+        node.id = counter++
+        if (node.left) {
+          queue.push(node.left)
+        }
+        if (node.right) {
+          queue.push(node.right)
+        }
+      }
+    }
+  }
+
+  function buildTreeHelper(A: ElementProp[], root: TreeNodeProp): TreeNodeProp | null {
     if (A.length <= 1) {
       return null
     }
@@ -16,15 +47,32 @@ export function buildTree(A: number[]): TreeNodeProp {
     const left = A.slice(0, middle)
     const right = A.slice(middle)
 
-    root.left = { A: structuredClone(left), left: null, right: null}
+    root.left = {id: -1, A: structuredClone(left), left: null, right: null, visible: false}
     buildTreeHelper(left, root.left)
-    root.right = {A: structuredClone(right), left: null, right: null}
+    root.right = {id: -1, A: structuredClone(right), left: null, right: null, visible: false}
     buildTreeHelper(right, root.right)
 
     return root
   }
 
-  buildTreeHelper(A, root)
+  buildTreeHelper(elements, root)
+  generateId(root)
 
   return root
+}
+
+function toggleNodeVisibility(node : TreeNodeProp | null, id : number) {
+    if (!node) return;
+
+    if (node.id === id) {
+        node.visible = !node.visible
+    }
+}
+
+function toggleElementVisiblity(node : TreeNodeProp | null, id : number, index : number) {
+    if (!node) return;
+    
+    if (node.id === id) {
+        node.A[index].visible = !node.A[index].visible
+    }
 }

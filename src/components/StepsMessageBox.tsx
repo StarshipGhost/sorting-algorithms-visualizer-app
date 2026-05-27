3d917d9cf90e891406ab28b155e2a8090b69061d
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2026-05-27 01:07:50 -0400

    Change box's background color

--- src/components/StepsMessageBox.tsx
+++ src/components/StepsMessageBox.tsx
@@ -1,6 +1,6 @@
 const StepsMessageBox = ({message}: {message: string}) => {
   return (
-    <div className="max-w-xl mx-auto bg-blue-200/60 p-4 mb-8 border border-solid border-blue-300 rounded-sm">
+    <div className="max-w-lg h-25 mx-auto bg-blue-200/30 p-4 mb-8 border border-solid border-blue-200 rounded-sm">
       <div className="border-l-5 border-solid border-blue-400 rounded-sm pl-3">
         <p className="font-medium">Steps:</p>
         <p className="text-sm text-neutral-700">{message}</p>

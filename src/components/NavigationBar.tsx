72c5b4bb77a56535331ffa9b84b39a4c58d651e6
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2026-06-22 19:30:03 -0400

    Extract the navigation bar content into a component to make it reusable

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,28 +1,56 @@
-import { twMerge } from "tailwind-merge";
-import type { SortingAlgorithmProps } from "../utils/types";
+import {twMerge} from 'tailwind-merge'
+import type {SortingAlgorithmProps} from '../utils/types'
 
-const NavigationBar = ({ algorithms, currentId, onIdChange, mode }: { algorithms: SortingAlgorithmProps[]; currentId: number; onIdChange: (id: number) => void; mode: "simulation" | "manual" }) => {
+export const AlgorithmItems = ({
+  algorithms,
+  currentId,
+  onIdChange,
+  mode,
+}: {
+  algorithms: SortingAlgorithmProps[]
+  currentId: number
+  onIdChange: (id: number) => void
+  mode: 'simulation' | 'manual'
+}) => {
   return (
-    <div className="p-4">
+    <>
       <h1 className="text-xl text-neutral-200 border-b-4 border-sky-900/50 whitespace-nowrap pb-2">Sorting Algorithms</h1>
       <ul className="py-4 space-y-2">
-        {algorithms.map(({ id, introduction: { title } }) => (
+        {algorithms.map(({id, introduction: {title}}) => (
           <li
             key={title}
             className={twMerge(
               `text-neutral-400 text-md font-medium px-4 py-2 rounded-sm cursor-pointer hover:text-white hover:bg-neutral-800 whitespace-nowrap`,
-              `${currentId === id && "text-white bg-sky-600 hover:bg-sky-600/90"}`,
-              `${mode === "simulation" && "brightness-50 pointer-events-none cursor-default"}`,
+              `${currentId === id && 'text-white bg-sky-600 hover:bg-sky-600/90'}`,
+              `${mode === 'simulation' && 'brightness-50 pointer-events-none cursor-default'}`,
             )}
             onClick={() => onIdChange(id)}
-            aria-disabled={mode === "simulation"}
+            aria-disabled={mode === 'simulation'}
           >
             {title}
           </li>
         ))}
       </ul>
-    </div>
-  );
-};
+    </>
+  )
+}
 
-export default NavigationBar;
+const NavigationBar = ({
+  algorithms,
+  currentId,
+  onIdChange,
+  mode,
+}: {
+  algorithms: SortingAlgorithmProps[]
+  currentId: number
+  onIdChange: (id: number) => void
+  mode: 'simulation' | 'manual'
+}) => {
+  return (
+    <div className="fixed w-1/5 p-6 pr-0 hidden lg:block ">
+      <AlgorithmItems algorithms={algorithms} currentId={currentId} onIdChange={onIdChange} mode={mode} />
+    </div>
+  )
+}
+
+export default NavigationBar

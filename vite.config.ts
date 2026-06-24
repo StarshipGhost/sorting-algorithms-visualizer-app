668ddc37da95038a1e7900b17952a6c3261938a5
Saad Kalyati <saad.kalyati.1@ens.etsmtl.ca>
2026-06-24 00:37:24 -0400

    Add base url for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/sorting-algorithms-visualizer-app/',
   plugins: [react(), tailwindcss()],
 })
